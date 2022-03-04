15fa5c70c4ac9afa197c37be0349fcba2c3d2fb7
4yd1K <papirys13@yandex.by>
2022-03-04 16:17:41 +0300

    feat: add HW 2

--- Lesson 1/index.js
+++ Lesson 1/index.js
@@ -1,16 +1,83 @@
+// lesson 2
+
+// шахматная доска
+
+function desk (){
+    let a1 = "# # # # ";
+    let a2 = " # # # #";
+    for(let i = 0; i < 4 ;i++){
+    console.log(a1);
+    console.log(a2);
+    }
+    }
+    desk()
+
+// факториал (без рекурсии)
+    function fuk (n) {
+        let f=1;
+        for (let i = 1; i <= n; i++)
+        f = f * i
+        return f;
+
+    }
+
+    console.log(fuk(10))
+
+// сортировка пузырьками, без сорт
+
+    var m = [7, 8, 3, 10, 5, 1, 4, 9, 2, 6];
+ 
+var count = m.length-1;
+ 
+for (var i = 0; i < count; i++) {
+     for (var j = 0; j < count-i; j++) {
+ 
+        if (m[j] > m[j+1]) {
+           var max = m[j];
+           m[j] = m[j+1];
+           m[j+1] = max;
+        }
+     }  
+}
+ 
+console.log(m);
+
+// size Converter
+
+
+function sizeConverter (size , unit) {
+if (unit === "Кб") {
+    return size*1024
+} if (unit === "Мб") {
+    return size*1024*1024
+} if (unit === "Гб") {
+    return size*1024*1024*1024
+}
+
+}
+console.log (sizeConverter(10, "Мб"))
+
+
+
+
+// lesson 1
+
+
 // console.log('Hello word');
 
+// task 1
 let str = '123456'
 
 console.log(str.split('').reverse().join(''));
 
-
+// task 2
 let string = 'http://my-first-site.com'
 
-console.log(string.startsWith('http://') && string.endsWith('.html'))
+console.log(string.startsWith('http://') && string.endsWith('.html'));
 
 
 
+// task 3
 
 const dayOfWeek = function(number){
     const week = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье']
@@ -19,3 +86,7 @@ const dayOfWeek = function(number){
 }
 
 console.log(dayOfWeek(1))
+
+
+
+
